db7f33cfd14809dcfde288f01dbd3bea72097513
Rahuldeb5 <rahuldeb.usa@gmail.com>
2025-07-07 14:11:12 -0400

    Added .gitignore and way to extract zip files from dataset

---
+++ src/utils/extract_zipfiles.py
@@ -0,0 +1,14 @@
+import zipfile
+import os
+
+data_dir = "/Data"
+
+for file in os.listdir(data_dir):
+    if(file.endswith(".zip")):
+        zip_path = os.path.join(data_dir, file)
+        extract_folder = os.path.join(data_dir, file.replace(".zip", ""))
+
+        os.makedirs(extract_folder, exist_ok=True)
+
+        with zipfile.ZipFile(zip_path, 'r') as zip_reader:
+            zip_reader.extractall(extract_folder)
